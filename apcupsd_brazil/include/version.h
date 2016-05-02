94875e20c1e67df0ee913574477a2edf48acbad8
wpopov <wpopov@gmail.com>
2016-05-02 01:59:09 +0000

    windows usb changes

--- apcupsd_brazil/include/version.h
+++
@@ -1,9 +0,0 @@
-#define DEBUG 1
-#define VERSION "0.5.5"
-#define ADATE   "26 April 2016"
-
-#define APCUPSD_RELEASE VERSION
-
-#ifndef APCUPSD_HOST
-# define APCUPSD_HOST HOST
-#endif
--- apcupsd_brazil/src/win32/compat/netdb.h
+++
@@ -1,3 +0,0 @@
-// winsock2.h has gethostbyname
-#include <windows.h>
-#include <winsock2.h>
